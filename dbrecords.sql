a29187292bb5a1fa760835c736f19bebfa9e0450
Lia Neville <lianeville5@gmail.com>
2021-07-21 19:13:25 -0400

    adds gregory's email

--- dbrecords.sql
+++ dbrecords.sql
@@ -10,7 +10,7 @@ truncate `users`;
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'liaaa', 'lianeville5@gmail.com', '123', 'lia', '2021-07-21 17:50:09');
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'andrewwww', 'abuchholz33@gmail.com', '123', 'andrew', '2021-07-21 17:55:09');
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'aeastraeaeaea', 'abombita98@hotmail.com', '123', 'astraea', '2021-07-21 17:55:09');
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'gregoryyy', 'gregory@gmail.com', '123', 'gregory', '2021-07-21 17:55:09');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'gregoryyy', 'gdixon5125@gmail.com', '123', 'gregory', '2021-07-21 17:55:09');
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'ericcc', 'erh@theclubhou.se', '123', 'eric', '2021-07-21 17:55:09');
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'cookies', 'cookies@gmail.com', '123', 'someone', '2021-07-21 15:50:09');
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `display_name`, `created_on`) VALUES (NULL, 'unfriendly', 'unfriendly@gmail.com', '123', 'evil', '2021-07-21 17:55:09');
