07b2705d708b39c03c6388b04ecf28f3df450098
Andrew Buchholz <learningemail33@gmail.com>
2021-07-21 14:06:32 -0400

    moved tweet bar to center column

---
+++ dbschema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `crits` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `crit_id` int(11) DEFAULT NULL,
+    `message` text NOT NULL,
+    `created_on` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `crit_likes` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `crit_id` int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `created_on` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `display_name` varchar(255) NOT NULL,
+    `created_on` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4
