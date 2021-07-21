fc7170f2ba6b4c985e5baa8f93ab133b0fd2e391
Lia Neville <lianeville5@gmail.com>
2021-07-21 17:13:18 -0400

    adds mysql database env connections

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 require('dotenv').config();
 
 app.use( express.static('static') );
 app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
         res.render('index')
 })
+
+var connection = mysql.createConnection({
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : 'critter',
+        port: 3306
+});
+connection.connect();
 
 app.listen( process.env.PORT || 8080);
