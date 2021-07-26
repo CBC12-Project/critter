4539907ee8990d084a6a2b946dfac88bca977a10
Astraea Bombita <abombita98@hotmail.com>
2021-07-26 18:43:08 -0400

    starting routes

---
+++ static/js/crit.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(event){
+
+    document.getElementsById("1_crit").addEventListener('click', function(event){
+        if (event.target.classList.contains('fas fa-bug')){
+        
+        };
+    });
+})
