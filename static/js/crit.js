6246ff5baa7a3b2225f6b105dc4ed7feaa22dc5f
Astraea Bombita <abombita98@hotmail.com>
2021-07-28 16:46:22 -0400

    i broke the likes

--- static/js/crit.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(event){
-
-    document.getElementsById("1_crit").addEventListener('click', function(event){
-        if (event.target.classList.contains('fas fa-bug')){
-        
-        };
-    });
-})
